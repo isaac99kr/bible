af9111de8b00ab9c3252c3aabb0ef7bcc7f7bf8c
unknown <isaac99kr@gmail.com>
2020-05-04 14:57:10 +0700

    Updates

---
+++ precache-manifest.104456c59270ff50daf4eb986b16edbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba184a45b496339b8ae3c7bd5276710",
+    "url": "/bible/index.html"
+  },
+  {
+    "revision": "afc14f0948656684686a",
+    "url": "/bible/static/css/main.11fe4b5c.chunk.css"
+  },
+  {
+    "revision": "a66cf91573c26a77c33f",
+    "url": "/bible/static/js/2.2d823c53.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/bible/static/js/2.2d823c53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afc14f0948656684686a",
+    "url": "/bible/static/js/main.87717b75.chunk.js"
+  },
+  {
+    "revision": "012c1bd887673b4172bd",
+    "url": "/bible/static/js/runtime-main.b8364009.js"
+  }
+]);
